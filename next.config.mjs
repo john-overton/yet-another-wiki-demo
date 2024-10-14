c9cf43b67111263f24f82020acbd5c202714980e
john-overton <john@jroverton.com>
2024-10-14 18:37:30 -0500

    base commit for base-app-custom branch

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,6 @@
-import nextra from 'nextra'
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  // You can add custom config options here
+}
 
-const withNextra = nextra({
-  theme: 'nextra-theme-docs',
-  themeConfig: './theme.config.jsx',
-  defaultShowCopyCode: true
-})
-
-export default withNextra({
-  // Additional Next.js config options can be added here
-})
+export default nextConfig
