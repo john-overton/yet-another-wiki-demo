6331de8ac9d5fd244f60c7e9513d8b31165f5c1e
john-overton <john@jroverton.com>
2024-10-29 19:41:54 -0500

    added copyright to footer

--- app/components/ClientLayout.js
+++ app/components/ClientLayout.js
@@ -36,8 +36,8 @@ export function ClientLayout({ children }) {
       <main className="flex-1 z-1 overflow-auto">
         {children}
       </main>
-      <footer className="h-12 p-1 text-center bg-gray-100 dark:bg-gray-800 transition-colors duration-200 border-gray-footer">
-        <p>Footer</p>
+      <footer className="h-12 p-1 text-sm text-center bg-gray-100 dark:bg-gray-800 transition-colors duration-200 border-gray-footer">
+        <p>© 2024 - Yet Another Wiki - All Rights Reserved</p>
       </footer>
     </div>
   );
